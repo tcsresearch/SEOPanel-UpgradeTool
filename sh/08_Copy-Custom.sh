029dff75addfa65b1617ffbad04918e2421d75ee
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2026-04-30 03:45:42 -0500

    Enhance copy script with verbose output
    Added verbose output for copying custom plugins, themes, and uploads.

--- sh/08_Copy-Custom.sh
+++ sh/08_Copy-Custom.sh
@@ -1,8 +1,16 @@
+#!/usr/bin/env bash
+
 # Copy custom plugins
-cp -r seopanel_old_*/plugins/custom_* seopanel/plugins/
+echo "Copying custom plugins..."
+cp -rv seopanel_old_*/plugins/custom_* seopanel/plugins/
+echo " "
 
 # Copy custom themes (if any)
-cp -r seopanel_old_*/themes/custom_* seopanel/themes/
+echo "Copying custom themes (if any)..."
+cp -rv seopanel_old_*/themes/custom_* seopanel/themes/
+echo " "
 
 # Copy any custom uploads or data
-cp -r seopanel_old_*/uploads/* seopanel/uploads/
+echo "Copying any custom uploads or data..."
+cp -rv seopanel_old_*/uploads/* seopanel/uploads/
+echo " "
