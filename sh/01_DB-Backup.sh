5eeeb47582bf3a19a3063bc7e247c385f54bf148
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2026-05-20 03:31:41 -0500

    Fix quotes in backup filename generation

--- sh/01_DB-Backup.sh
+++ sh/01_DB-Backup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Using mysqldump
-mysqldump -u seopanel_user -p seopanel_db > seopanel_backup_$(date +%Y%m%d_%H%M%S).sql
+mysqldump -u seopanel_user -p seopanel_db > seopanel_backup_"$(date +%Y%m%d_%H%M%S)".sql
 
 # Or using MySQL Workbench / phpMyAdmin export feature
