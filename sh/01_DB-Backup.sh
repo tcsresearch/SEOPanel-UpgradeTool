e5b880ae8395a3107cd4297b773ddb01e13b82ad
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2026-05-31 20:37:36 -0500

    Add comment for backup folder name definition
    Added a comment to define the backup folder name.

--- sh/01_DB-Backup.sh
+++ sh/01_DB-Backup.sh
@@ -2,6 +2,7 @@
 
 # Using mysqldump
 # TODO: Define $seopanel_user and $seopanel_db.
+#       Define backup folder name.
 mysqldump -u $seopanel_user -p $seopanel_db > seopanel_backup_"$(date +%Y%m%d_%H%M%S)".sql
 
 # Or using MySQL Workbench / phpMyAdmin export feature
