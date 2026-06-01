ecaf7ebbf461ae27bd96794788352f34162c9e8f
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2026-05-31 20:36:56 -0500

    Use variables for database user and name in backup script
    Replaced hardcoded database user and name with variables.

--- sh/01_DB-Backup.sh
+++ sh/01_DB-Backup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # Using mysqldump
-mysqldump -u seopanel_user -p seopanel_db > seopanel_backup_"$(date +%Y%m%d_%H%M%S)".sql
+# TODO: Define $seopanel_user and $seopanel_db.
+mysqldump -u $seopanel_user -p $seopanel_db > seopanel_backup_"$(date +%Y%m%d_%H%M%S)".sql
 
 # Or using MySQL Workbench / phpMyAdmin export feature
